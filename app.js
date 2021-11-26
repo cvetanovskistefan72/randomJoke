94d93f8d97553c0aba5ac2f65d47d00b18b3dd2e
Stefan Cvetanovski <stefan.cvetanovski@polarcape.com>
2021-11-26 14:20:08 +0100

    Implement CORS

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const jokeRouter = require("./routes/jokeRoutes");
 const userRouter = require("./routes/userRoutes");
 const AppError = require("./utils/AppError");
+var cors = require("cors");
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/jokes", jokeRouter);
 app.use("/api/user", userRouter);
